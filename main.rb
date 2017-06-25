bedff299e02972c78178267577eb29034ed8fd38
Gleb1Spice <gleb.yazykov@gmail.com>
2017-06-25 14:30:38 +0300

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,18 +1,22 @@
-words = ["spice", "fff",  "sdasddsa", "qweqweasd", "asdqkfqwf", "qweqweas22d", "qweqwasdasdasdassd"] 
+words = ["spice", "fff",  "sdasddsa", "qweqweasd", "asdqkfqwf", "qweqweas22d", "qweqwasdasdasdassds"] 
 
 
 def method(words)
   max=""
+  max_size = 0
   words.each do |w| 
     if w.size>max.size
-      max=w  
+      max=w
+      max_size=w.size
     end
-    
   end
-	max
+  if (max_size % 2 == 1)
+    max="Nechetnoe (nill)"
+  else	
+  end
+  max
 end
 
 result = method(words)
 
 p "the longest even word is: #{result}"
-
